e8fbd22fae21c2354c0f110529f3afb064f2baf3
Clecio Bachini <cbachini@gmail.com>
2024-12-19 18:58:19 -0300

    tasting codes

--- includes/class-sz-conectar-idb.php
+++ includes/class-sz-conectar-idb.php
@@ -7,27 +7,17 @@ if (!class_exists('Sz_Conectar_Idb')) {
 
         public function __construct() {
             $this->load_dependencies();
-            $this->define_ajax_hooks();
             $this->define_admin_hooks();
             $this->define_shortcodes();
         }
 
         private function load_dependencies() {
             require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-sz-conectar-idb-loader.php';
-            require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-sz-conectar-idb-shortcodes.php';
-
             $this->loader = new Sz_Conectar_Idb_Loader();
         }
 
-        private function define_ajax_hooks() {
-            $this->loader->add_action('wp_ajax_gerar_charada_ajax', 'Sz_Conectar_Idb_Shortcodes', 'gerar_charada_ajax');
-            $this->loader->add_action('wp_ajax_nopriv_gerar_charada_ajax', 'Sz_Conectar_Idb_Shortcodes', 'gerar_charada_ajax');
-            $this->loader->add_action('wp_ajax_validar_charada_resposta', 'Sz_Conectar_Idb_Shortcodes', 'validar_charada_resposta');
-            $this->loader->add_action('wp_ajax_nopriv_validar_charada_resposta', 'Sz_Conectar_Idb_Shortcodes', 'validar_charada_resposta');
-        }
-
         private function define_admin_hooks() {
-            $this->loader->add_action('admin_menu', 'Sz_Conectar_Idb_Admin', 'add_admin_menu');
+            $plugin_admin = new SZ_Conectar_IDB_Admin('sz-conectar-idb', SZ_CONECTAR_IDB_VERSION);
         }
 
         private function define_shortcodes() {
