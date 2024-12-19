b6ca7a547d13ba8eaaae15b92352092f67c3fb3f
Clecio Bachini <cbachini@gmail.com>
2024-12-19 17:56:28 -0300

    tasting codes

--- includes/class-sz-conectar-idb-codes.php
+++ includes/class-sz-conectar-idb-codes.php
@@ -1,34 +1,71 @@
 <?php
 
-class Sz_Conectar_Idb_Codes {
-    public static function init() {
-        add_action('wp_ajax_validar_codigo_acesso', [self::class, 'validar_codigo_acesso']);
-        add_action('wp_ajax_nopriv_validar_codigo_acesso', [self::class, 'validar_codigo_acesso']);
+class Sz_Conectar_Idb {
+
+    protected $loader;
+    protected $plugin_name;
+    protected $version;
+
+    public function __construct() {
+        if (defined('SZ_CONECTAR_IDB_VERSION')) {
+            $this->version = SZ_CONECTAR_IDB_VERSION;
+        } else {
+            $this->version = '1.0.0';
+        }
+        $this->plugin_name = 'sz-conectar-idb';
+
+        $this->load_dependencies();
+        $this->set_locale();
+        $this->define_admin_hooks();
+        $this->define_public_hooks();
+        $this->define_shortcodes();
     }
 
-    public static function validar_codigo_acesso() {
-        global $wpdb;
+    private function load_dependencies() {
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-sz-conectar-idb-loader.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-sz-conectar-idb-i18n.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-sz-conectar-idb-admin.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-sz-conectar-idb-public.php';
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-sz-conectar-idb-shortcodes.php';
 
-        $codigo = isset($_POST['codigo']) ? sanitize_text_field($_POST['codigo']) : '';
-        if (!empty($codigo)) {
-            $table_name = $wpdb->prefix . 'sz_access_codes';
-            $code = $wpdb->get_row($wpdb->prepare(
-                "SELECT * FROM $table_name WHERE access_code = %s AND is_active = 1",
-                $codigo
-            ));
+        $this->loader = new Sz_Conectar_Idb_Loader();
+    }
 
-            if ($code) {
-                if ($code->current_uses >= $code->max_uses) {
-                    wp_send_json_error(__('Código de acesso inválido ou já utilizado.', 'sz-conectar-idb'));
-                } else {
-                    $wpdb->update($table_name, ['current_uses' => $code->current_uses + 1], ['id' => $code->id]);
-                    wp_send_json_success(__('Código de acesso válido.', 'sz-conectar-idb'));
-                }
-            } else {
-                wp_send_json_error(__('Código de acesso inválido.', 'sz-conectar-idb'));
-            }
-        } else {
-            wp_send_json_error(__('Código não fornecido.', 'sz-conectar-idb'));
-        }
+    private function set_locale() {
+        $plugin_i18n = new Sz_Conectar_Idb_i18n();
+        $this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
+    }
+
+    private function define_admin_hooks() {
+        $plugin_admin = new Sz_Conectar_Idb_Admin($this->get_plugin_name(), $this->get_version());
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
+    }
+
+    private function define_public_hooks() {
+        $plugin_public = new Sz_Conectar_Idb_Public($this->get_plugin_name(), $this->get_version());
+        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
+        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
+    }
+
+    private function define_shortcodes() {
+        // Registro de shortcodes diretamente com WordPress
+        add_shortcode('gerar_charada', ['Sz_Conectar_Idb_Shortcodes', 'gerar_charada_shortcode']);
+    }
+
+    public function run() {
+        $this->loader->run();
+    }
+
+    public function get_plugin_name() {
+        return $this->plugin_name;
+    }
+
+    public function get_loader() {
+        return $this->loader;
+    }
+
+    public function get_version() {
+        return $this->version;
     }
 }
